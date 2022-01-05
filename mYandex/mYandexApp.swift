fe428faef9648928aaa985770a015051164c48cb
Victoria Belinschi <victoria.belinschi@endava.com>
2022-01-05 11:05:31 +0200

    Initial commit

---
+++ mYandex/mYandexApp.swift
@@ -0,0 +1,17 @@
+//
+//  mYandexApp.swift
+//  mYandex
+//
+//  Created by Victoria Belinschi on 05.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct mYandexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
